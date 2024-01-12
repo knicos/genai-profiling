88b3c9e218f14333ec01c9cdd6e653bc7c1f5051
Nicolas Pope <npope@uef.fi>
2024-01-12 08:39:30 +0000

    Merged PR 271: #368 Add more content
    Add more content
    
    Related work items: #368

--- src/components/Slide/QuestionSlide.tsx
+++ src/components/Slide/QuestionSlide.tsx
@@ -1,9 +1,17 @@
 import { SlideQuestion } from './types';
+import style from './style.module.css';
 
 interface Props {
     meta: SlideQuestion;
 }
 
 export default function QuestionSlide({ meta }: Props) {
-    return <div style={{ fontSize: `${meta.fontSize || 1}em` }}>{meta.text}</div>;
+    return (
+        <div
+            className={style.questionSlide}
+            style={{ fontSize: `${meta.fontSize || 1}em` }}
+        >
+            {meta.text}
+        </div>
+    );
 }
